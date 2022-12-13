925e0a2c97e0e2ff538e922f35e9044d5c00d151
Neel Mehta <neelmehta00@gmail.com>
2022-12-13 16:32:10 -0500

    bfs - neel and ryuno

--- src/utils/SearchAlgorithms.js
+++ src/utils/SearchAlgorithms.js
@@ -38,8 +38,45 @@ export const dfs = async (startNode, endNode, graph, setEditedGraph) => {
   }
 };
 
+
 // TODO: implement (breadth first search)
-export const bfs = () => {};
+export const bfs = async (startNode, endNode, graph, setEditedGraph) => {
+  const explored = new Set();
+  let frontier_queue = [startNode];
+
+  while (frontier_queue.length !== 0) {
+    const newGraphObject = { ...graph };
+    const node_id = frontier_queue.shift();
+    explored.add(node_id);
+
+    const newNodes = graph.nodes.map((nd) =>
+      explored.has(nd.id)
+        ? {
+            ...nd,
+            color: "orange",
+          }
+        : nd,
+    );
+
+    newGraphObject.nodes = newNodes;
+    setEditedGraph(newGraphObject);
+
+    // Break if found
+    if (node_id === endNode) {
+      break;
+    }
+
+    // Else, search neighbors
+    const neighbors = graph.edges.flatMap((edge) =>
+      edge.from === node_id && !explored.has(edge.to) ? [edge.to] : [],
+    );
+
+    frontier_queue = [...neighbors, ...frontier_queue];
+
+    // Wait half a second before the next step
+    await sleep(500);
+  }
+};
 
 // TODO: implement (uniform-cost search)
 export const ucs = () => {};
